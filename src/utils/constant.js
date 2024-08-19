263d97b1864359bde6149e8fe000b7a38dbba8dd
Baibhav Saxena <baibhav91939@CELSYSNCR000169.local>
2024-08-19 21:00:48 +0530

    tesla API updated

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
 const API_KEY = process.env.REACT_APP_BLOG_API_KEY
 export const WALL_STREET_API = 'https://newsapi.org/v2/everything?domains=wsj.com&apiKey='+API_KEY
 
-export const TESLA_NEWS_API = 'https://newsapi.org/v2/everything?q=tesla&from=2024-07-10&sortBy=publishedAt&apiKey='+API_KEY
+export const TESLA_NEWS_API = 'https://newsapi.org/v2/everything?q=tesla&from=2024-07-19&sortBy=publishedAt&apiKey='+API_KEY
